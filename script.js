0ddc63a888dbc98664a784de65b4160b4fd8075f
spiritcoder666 <144348608+spiritcoder666@users.noreply.github.com>
2024-10-20 15:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,62 @@ SolvePuzzle.onclick = () => {
 	SudokuSolver(board, 0, 0, 9);
 };
 
-function SudokuSolver(board, i, j, n) {
-	// Write your Code here
+function isValid( board ,  i,  j,  num, n){
+    //row check
+    for(let x=0;x<n;x++){
+        if(board[i][x]==num){
+            return false;
+        }
+    }
+    //column check
+    for(let x=0;x<n;x++){
+        if(board[x][j]==num){
+            return false;
+        }
+    }
+   
+    //sub,matrix check
+     let rn= Math.sqrt(n);
+    let si= i- i%rn;
+    let sj = j- j%rn;
+    for(let x=si;x<si+rn;x++){
+        for(let y=sj;y<sj+rn;y++){
+            if(board[x][y]==num){
+                return false;
+            }
+        }
+    }
+    return true;
+    
+}
+
+function sudokusolver( board,i ,  j, n){
+    //base case 
+    if(i==n){
+       // print(board,n);
+	   FillBoard(board)
+        return true;
+    }
+    //if we r not inside the board
+    if(j==n){
+      return   sudokusolver(board, i+1,0,n);
+    }
+    //if cell is already filled -> just  move ahead
+    if (board[i][j]!=0){
+       return  sudokusolver(board,i,j+1,n);
+    }
+    //we try to fill te cell with approprite no.
+    for(let num=1;num<=9;num++){
+       if( isValid(board,i,j,num,n)){
+           board[i][j]=num;
+          let subans = sudokusolver(board,i,j+1,n);
+          if(subans){
+              return true;
+          }
+          //backtracking -> undo the changes 
+          board[i][j]=0;
+          
+       }
+    }
+    return false;
 }
